f994bd4471e807a901e5e061b855bec8dae1c2e2
vadym <vadikhide@yandex.ru>
2022-11-27 01:59:41 +0200

    gf

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,12 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        HashMap<String,Integer> map= new HashMap<>();
+        map.put("Вася",+7675);
+        map.put("Петя",+7175);
+        map.put("Вова",+7375);
+
+        System.out.println(map.keySet()+":"+map.values());
     }
 }
-
